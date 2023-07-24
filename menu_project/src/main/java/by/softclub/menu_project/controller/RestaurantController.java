69230f876a139cf2a260a698cdb0de8b87bb610e
Ерощенко Денис Викторович <dzianis.yaroshchanka@softclub.by>
2023-07-24 16:46:10 +0300

    thymeleaf

--- menu_project/src/main/java/by/softclub/menu_project/controller/RestaurantController.java
+++ menu_project/src/main/java/by/softclub/menu_project/controller/RestaurantController.java
@@ -3,10 +3,13 @@ package by.softclub.menu_project.controller;
 import by.softclub.menu_project.entity.Restaurant;
 import by.softclub.menu_project.repository.RestaurantRepository;
 import by.softclub.menu_project.service.RestaurantServiceImpl;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,29 +24,43 @@ public class RestaurantController {
     private final RestaurantServiceImpl restaurantService;
 
 
-    @PostMapping
-    public ResponseEntity<Restaurant> addRestaurant(@RequestBody Restaurant restaurant) {
-        Restaurant savedRestaurant = restaurantService.addRestaurant(restaurant);
-        return ResponseEntity.ok(savedRestaurant);
+//    @PostMapping("/add")
+//    public ResponseEntity<Restaurant> addRestaurant(@RequestBody Restaurant restaurant) {
+//        Restaurant savedRestaurant = restaurantService.addRestaurant(restaurant);
+//        return ResponseEntity.ok(savedRestaurant);
+//    }
+
+    
+    @PostMapping("/addRest")
+    public String addRestaurant(@Valid Restaurant rest, BindingResult result, Model model){
+        if(result.hasErrors()){
+            return "add-rest";
+        }
+        restaurantService.addRestaurant(rest);
+        return "redirect:/index";
     }
 
-    @GetMapping("/all")
-    public List<Restaurant> getAllRestaurants() {
-        return restaurantService.getRestaurants();
+    @GetMapping
+    public String getAllRestaurants(Model model){
+        model.addAttribute("restaurants", restaurantService.getRestaurants());
+        return "index";
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/get/{id}")
     public Optional<Restaurant> getRestaurant(@PathVariable("id") Long id) {
         return restaurantService.getRestaurant(id);
     }
 
-    @PutMapping("/{restId}")
-    public ResponseEntity<Restaurant> updateRestaurant(@RequestBody Restaurant restaurant, @PathVariable Long restId) {
-        Restaurant updatedRestaurant = restaurantService.updateRestaurant(restaurant, restId);
-        return ResponseEntity.ok(updatedRestaurant);
+    @PutMapping("/update/{id}")
+    public String updateRestaurant(@PathVariable("id") Long id, Model model) {
+        Restaurant rest = restaurantService.getRestaurant(id)
+                .orElseThrow(() -> new IllegalArgumentException("invalid user ID: " + id));
+
+        model.addAttribute("restaurant", rest);
+        return "update-user";
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteRestaurant(@PathVariable("id") Long restId) {
         try {
             restaurantService.deleteRestaurant(restId);
