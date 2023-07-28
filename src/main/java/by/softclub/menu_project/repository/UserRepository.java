95b27294dffec61d291b080d3b6c5026c969a0fb
Denis <79418860+denisero21@users.noreply.github.com>
2023-07-28 16:00:21 +0300

    Delete src directory

--- src/main/java/by/softclub/menu_project/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package by.softclub.menu_project.repository;
-
-import by.softclub.menu_project.entity.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
