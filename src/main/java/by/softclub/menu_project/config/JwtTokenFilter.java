9ac636f561e670e5b64b30c9ad84de8dd67bdef7
Ерощенко Денис Викторович <dzianis.yaroshchanka@softclub.by>
2023-08-17 12:28:34 +0300

    Actuator was added

---
+++ src/main/java/by/softclub/menu_project/config/JwtTokenFilter.java
@@ -0,0 +1,66 @@
+//package by.softclub.menu_project.config;
+//
+//import by.softclub.menu_project.service.UserDetailsServiceImpl;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import java.io.IOException;
+//
+//import static org.apache.logging.log4j.util.Strings.isEmpty;
+//
+//
+//@Component
+//@RequiredArgsConstructor
+//public class JwtTokenFilter extends OncePerRequestFilter {
+//
+//    private final JwtTokenUtil jwtTokenUtil;
+//    private final UserDetailsServiceImpl userDetailsService;
+//
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request,
+//                                    HttpServletResponse response,
+//                                    FilterChain chain)
+//            throws ServletException, IOException {
+//        // Get authorization header and validate
+//        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+//        if (isEmpty(header) || !header.startsWith("Bearer ")) {
+//            chain.doFilter(request, response);
+//            return;
+//        }
+//
+//        // Get jwt token and validate
+//        final String token = header.split(" ")[1].trim();
+//        if (!jwtTokenUtil.validate(token)) {
+//            chain.doFilter(request, response);
+//            return;
+//        }
+//
+//        // Get user identity and set it on the spring security context
+//        UserDetails userDetails = userDetailsService
+//                .loadUserByUsername(jwtTokenUtil.getUsername(token))
+//                .orElse(null);
+//
+//        UsernamePasswordAuthenticationToken
+//                authentication = new UsernamePasswordAuthenticationToken(
+//                userDetails, null,
+//                userDetails == null ?
+//                        List.of() : userDetails.getAuthorities()
+//        );
+//
+//        authentication.setDetails(
+//                new WebAuthenticationDetailsSource().buildDetails(request)
+//        );
+//
+//        SecurityContextHolder.getContext().setAuthentication(authentication);
+//        chain.doFilter(request, response);
+//    }
+//
+//}
--- src/main/java/by/softclub/menu_project/service/UserDetailsServiceImpl.java
+++ src/main/java/by/softclub/menu_project/service/UserDetailsServiceImpl.java
@@ -1,49 +1,49 @@
-package by.softclub.menu_project.service;
-
-import by.softclub.menu_project.entity.user.User;
-import by.softclub.menu_project.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
-
-@RequiredArgsConstructor
-@Service
-@Slf4j
-public class UserDetailsServiceImpl implements UserDetailsService {
-
-    private final UserRepository userRepository;
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email);
-
-        if (user == null) {
-            log.info("User with email " + email + " not found...");
-            throw new UsernameNotFoundException("");
-        } else {
-            return new org.springframework.security.core.userdetails.User(
-                    user.getEmail(),
-                    user.getPassword(),
-                    true,
-                    true,
-                    true,
-                    true,
-                    getAuthorities(user));
-        }
-    }
-
-    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
-        return user.getRoles()
-                .stream()
-                .flatMap(role -> role.getRolePrivileges().stream())
-                .map(privilege -> new SimpleGrantedAuthority(privilege.getName()))
-                .collect(Collectors.toList());
-    }
-}
+//package by.softclub.menu_project.service;
+//
+//import by.softclub.menu_project.entity.user.User;
+//import by.softclub.menu_project.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Collection;
+//import java.util.stream.Collectors;
+//
+//@RequiredArgsConstructor
+//@Service
+//@Slf4j
+//public class UserDetailsServiceImpl implements UserDetailsService {
+//
+//    private final UserRepository userRepository;
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//        User user = userRepository.findByEmail(email);
+//
+//        if (user == null) {
+//            log.info("User with email " + email + " not found...");
+//            throw new UsernameNotFoundException("");
+//        } else {
+//            return new org.springframework.security.core.userdetails.User(
+//                    user.getEmail(),
+//                    user.getPassword(),
+//                    true,
+//                    true,
+//                    true,
+//                    true,
+//                    getAuthorities(user));
+//        }
+//    }
+//
+//    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+//        return user.getRoles()
+//                .stream()
+//                .flatMap(role -> role.getRolePrivileges().stream())
+//                .map(privilege -> new SimpleGrantedAuthority(privilege.getName()))
+//                .collect(Collectors.toList());
+//    }
+//}
